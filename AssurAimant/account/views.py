2ac7089085fadb51a3559d183a0fde8e4fa98ad6
NICHIKU <puypeethan@gmail.com>
2026-01-28 15:44:16 +0100

    feat: Add Profile View and Modification

--- AssurAimant/account/views.py
+++ AssurAimant/account/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import DetailView, UpdateView, ListView
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
 
-# Create your views here.
+from django.contrib.auth import get_user_model
+
+from .forms import ModificationForm
+User = get_user_model()
+
+class UserProfileView(ListView):
+    model = User
+    template_name = 'account/profile.html'
+    context_object_name = 'profile'
+
+class AccountModificationView(UpdateView):
+    model = User
+    form_class = ModificationForm
+    template_name = 'account/profile_modif.html'
+    success_url = reverse_lazy('profile')
+    
+    def get_object(self):
+        return self.request.user
+    
+    def post(self, request):
+        form = ModificationForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+        return render(request, 'account/profile_modif.html', {'form': form})
+
+    
+
+
