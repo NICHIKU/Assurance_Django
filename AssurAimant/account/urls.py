fc837f47ab3cf495717c612d211a10d1c92e7efa
NICHIKU <puypeethan@gmail.com>
2026-01-28 16:04:57 +0100

    fix(account): Fixeds urls.py

--- AssurAimant/account/urls.py
+++ AssurAimant/account/urls.py
@@ -1,15 +1,8 @@
 from django.urls import path
-<<<<<<< HEAD
-from .views import UserProfileView, AccountModificationView
+from .views import UserProfileView, AccountModificationView, views
 
 urlpatterns = [
     path('profile/', UserProfileView.as_view(), name='profile'),
     path('profile/modif/', AccountModificationView.as_view(), name='profile_modif'),
-]
-=======
-from . import views
-
-urlpatterns = [
     path('register', views.RegisterView.as_view(), name="register")
 ]
->>>>>>> f83379f781fe660dab62e3d594c1f81d77f27ab1
