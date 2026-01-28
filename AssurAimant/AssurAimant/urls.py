66c13503eafa33219cc42eb2a1b3611c4ded3fc6
Slyxi7 <riem.baptiste0705@gmail.com>
2026-01-28 16:45:40 +0100

    feat(home): I have created the base.html correctly (just navbar & footer) and started my app home, which is currently displayed without any style.

--- AssurAimant/AssurAimant/urls.py
+++ AssurAimant/AssurAimant/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from account import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home_view, name='home'),
+    path('', include('home.urls')),
 ]
