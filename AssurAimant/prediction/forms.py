ab859aa9395ae848aab8d11fbc62df04728f75fe
Umberto Emonds <umberto.emonds@gmail.com>
2026-01-30 12:31:13 +0100

    feat(prediction): predict and display result

---
+++ AssurAimant/prediction/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+
+class PredictionForm(forms.Form):
+    age = forms.IntegerField(required=True, label="Age", max_value=120, min_value=18)
+    children = forms.IntegerField(required=True, label="Nombre d'enfants", max_value=20)
+    height = forms.FloatField(required=True, label="Taille", max_value=230)
+    weight = forms.FloatField(required=True, label="Poids", min_value=20, max_value=250)
+    smoker = forms.ChoiceField(label="Fumez-vous ?", choices=(
+        ("yes", "Oui"),
+        ("no", "Non"),
+    ))
+    sex = forms.ChoiceField(label="Sexe", choices=(
+        ("male", "Homme"),
+        ("female", "Femme"),
+    ))
+    region = forms.ChoiceField(label="Région", choices=(
+        ("northeast", "Nord-est"),
+        ("northwest", "Nord-ouest"),
+        ("southeast", "Sud-est"),
+        ("southwest", "Sud-ouest"),
+    ))
